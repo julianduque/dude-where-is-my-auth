d36bc5e010cd4838b4876d86cad6dea07ab3b0ef
Julian Duque <julianduquej@gmail.com>
2015-12-03 02:54:15 -0500

    Create a basic http application with express

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const path = require('path')
+const express = require('express')
+const port = process.env.PORT || 8080
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
